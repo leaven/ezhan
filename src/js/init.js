7990af918624c070cc9a882ce86824c78cac46f6
leaven <dlwelllee@gmail.com>
2016-04-24 23:52:39 +0800

    update

--- src/js/init.js
+++ src/js/init.js
@@ -36,48 +36,51 @@ var Dial = {
 		return parseFloat(cssAttr.match(reg) ? cssAttr.match(reg)[1]: 0);
 	},
 	rotate: function(e) {
-		alert('rotate');
-		//每次只能允许一个转盘旋转
-		if(this.rotateLock && this.rotateLock === true) {
-			return;
-		}
-		var $se = $(e.target);
-		if(!$se.hasClass('ball')) {
-			$se = $se.parents('.ball');
-		}
-		//如果已经高亮，点击无效
-		if($se.hasClass('high-light')) {
-			return;
-		}
-		this.rotateLock = true;
-		$circle = $se.parents('.circle');
-		
-		var deg = this.getRotate($se.css("transform"));
-		var baseDeg = this.getRotate($circle.css("transform"));
-		var rotateDeg = (360-baseDeg % 360) %360;
-		var attr = $circle.data('attr');
-		this.searchParams[attr] = $se.data('param');
-		$("."+attr+"-circle-bg").addClass("high-light");
-		$circle.find(".high-light").removeClass('high-light');
-		$circle.addClass('high-light');
-		$se.addClass('high-light');
-
-		var circleRotateDeg = baseDeg+this._settings.baseAngle+360-deg+rotateDeg;
-		$circle.css('-webkit-transform', 'rotateZ('+circleRotateDeg+'deg)' ).find('.circle-scale');
-		
-		$circle.find('.ball-name').forEach(function(ball) {
-			$(ball).css('-webkit-transform', 'rotateZ('+($(ball).data('baseangle')-circleRotateDeg)%360+'deg)');
-		}.bind(this));
-
-		$circle.on('webkitTransitionEnd', function() {
-			$circle.off('webkitTransitionEnd').removeClass('high-light')
-			// .css('transform', 'rotateZ('+circleRotateDeg+'deg)');
-			$("."+attr+"-circle-bg").removeClass("high-light");
-			this.rotateLock = false;
-			if(this.checkSearch()) {
-				this.search();
+		try{
+			//每次只能允许一个转盘旋转
+			if(this.rotateLock && this.rotateLock === true) {
+				return;
 			}
-		}.bind(this));
+			var $se = $(e.target);
+			if(!$se.hasClass('ball')) {
+				$se = $se.parents('.ball');
+			}
+			//如果已经高亮，点击无效
+			if($se.hasClass('high-light')) {
+				return;
+			}
+			this.rotateLock = true;
+			$circle = $se.parents('.circle');
+			
+			var deg = this.getRotate($se.css("-webkit-transform"));
+			var baseDeg = this.getRotate($circle.css("-webkit-transform"));
+			var rotateDeg = (360-baseDeg % 360) %360;
+			var attr = $circle.data('attr');
+			this.searchParams[attr] = $se.data('param');
+			$("."+attr+"-circle-bg").addClass("high-light");
+			$circle.find(".high-light").removeClass('high-light');
+			$circle.addClass('high-light');
+			$se.addClass('high-light');
+
+			var circleRotateDeg = baseDeg+this._settings.baseAngle+360-deg+rotateDeg;
+			$circle.css('-webkit-transform', 'rotateZ('+circleRotateDeg+'deg)' ).find('.circle-scale');
+			
+			$circle.find('.ball-name').forEach(function(ball) {
+				$(ball).css('-webkit-transform', 'rotateZ('+($(ball).data('baseangle')-circleRotateDeg)%360+'deg)');
+			}.bind(this));
+
+			$circle.on('webkitTransitionEnd', function() {
+				$circle.off('webkitTransitionEnd').removeClass('high-light')
+				// .css('transform', 'rotateZ('+circleRotateDeg+'deg)');
+				$("."+attr+"-circle-bg").removeClass("high-light");
+				this.rotateLock = false;
+				if(this.checkSearch()) {
+					this.search();
+				}
+			}.bind(this));
+		}catch(e){
+			alert(JSON.stringify(e));
+		}
 	},
 	search: function() {
 		console.log('search');
